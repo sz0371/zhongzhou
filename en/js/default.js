1130ad1475c3c6833d9a230e685ab65422570e24
wsz <15638117845@163.com>
2018-12-19 10:06:29 +0800

    第一次提交

---
+++ en/js/default.js
@@ -0,0 +1,34 @@
+/*蓝科企业网站系统PHP版
+http://www.lankecms.com*/
+
+$(function(){
+  //默认左右滚动
+  $("#s1").xslider({
+    unitdisplayed:7,
+    movelength:1,
+    unitlen:129,
+    autoscroll:2000
+  }); 
+})
+
+function qiehuan(num){
+  //var linum=document.getElementById('navli').getElementsByTagName('li').length-1;
+  var linum=document.getElementById('navli').getElementsByTagName('ul').length-1;
+  //alert(linum);
+  for(var id=0;id<=linum;id++){
+    if(id==num){
+      if(document.getElementById("qh_con"+id)!=null){
+        document.getElementById("qh_con"+id).style.display="block";  
+        if(document.getElementById("qh_con"+id).getElementsByTagName('li').length==0){
+         document.getElementById("qh_con"+id).innerHTML=document.getElementById("qh_con0").innerHTML;
+        }
+      } 
+      document.getElementById("mynav"+id).className="active";
+    }else{
+      if(document.getElementById("qh_con"+id)!=null){
+        document.getElementById("qh_con"+id).style.display="none";
+      }
+      document.getElementById("mynav"+id).className="";
+    }
+  }
+}
